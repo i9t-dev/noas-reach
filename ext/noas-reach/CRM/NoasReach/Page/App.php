b2984d4609d3520e93e50ee61b2f04eb58db1a5f
Christophe Pelé <geekarist@users.noreply.github.com>
2026-09-17 19:45:25 +0200

    Preliminary full-text hook work

--- ext/noas-reach/CRM/NoasReach/Page/App.php
+++ ext/noas-reach/CRM/NoasReach/Page/App.php
@@ -1,9 +1,12 @@
 <?php
 
 use CRM_NoasReach_ExtensionUtil as E;
+use Civi\Core\HookInterface;
 
-class CRM_NoasReach_Page_App extends CRM_Core_Page {
-  public function run() {
+class CRM_NoasReach_Page_App extends CRM_Core_Page
+{
+  public function run()
+  {
     CRM_Utils_System::setTitle(E::ts('Noah\'s Reach'));
 
     $this->assign('extensionUrl', E::url(''));
@@ -12,3 +15,23 @@ class CRM_NoasReach_Page_App extends CRM_Core_Page {
   }
 }
 
+class CRM_NoasReach_FullTextIndexHook implements HookInterface
+{
+  public function hook_civicrm_install()
+  {
+    // To do: Execute init script to create the full-text contents table
+  }
+
+  public function hook_civicrm_post(
+    $op,
+    $objectName,
+    $objectId,
+    &$objectRef = NULL,
+    $params = NULL
+  ) {
+    // To do: Update full-text contents table according to the operation
+    // To do: Insertion ⇒ Insert content
+    // To do: Update ⇒ Update content
+    // To do: Deletion ⇒ Delete content
+  }
+}
